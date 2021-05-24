0096c918823f289afec05c58608bf6a082d9df67
Dobrosav Vlaskovic <vlaskovic.dodo@gmail.com>
2021-05-24 18:07:28 +0200

    prijavljivanje admina, potvrda korisnika i oglasa

---
+++ Faza 5 implementacija/app/Views/Prikaz.php
@@ -0,0 +1,34 @@
+<html lang="sr">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+    <body>
+    <?php
+        echo "".anchor("Administrator/logout","Odjava")." admin";
+    ?>
+        <div class="container">
+            <div class="row">
+            <div class="col-sm-12"> <h1>Korisnici</h1></div>
+            </div>
+            <?php
+            foreach ($korisnici as $pet){
+                echo "<div class='row'>";
+                echo "<div class='col-sm-3'>&nbsp;</div>";
+                echo "<div class='col-sm-6 border border-dark'><h5>KorisnikID{$pet->getIdk()}</h5> {$pet->getUsername()}</div>";
+                echo "<div class='col-sm-3'>".anchor("Administrator/PotvrdaK/{$pet->getIdk()}","potvrdi Korisnika")."</div></div>";
+            }
+            ?>
+            <div class="row">
+                <div class="col-sm-12"> <h1>Oglasi</h1></div>
+            </div>
+            <?php
+            foreach ($oglasi as $pet){
+                echo "<div class='row'>";
+                echo "<div class='col-sm-3'>&nbsp;</div>";
+                echo "<div class='col-sm-6 border border-dark'><h5>{$pet->getTitle()}</h5> </br> {$pet->getText()}</div>";
+                echo "<div class='col-sm-3'>".anchor("Administrator/PotvrdaO/{$pet->getIdo()}","potvrdi oglas")."</div></div>";
+            }
+            ?>
+        </div>
+    </body>
+</html>
